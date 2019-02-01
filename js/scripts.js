1ee42e85842bfa99a877c8a93f51340409a41b5e
silvermistshadow <bmead72@gmail.com>
2019-02-01 10:51:20 -0800

    make it a toggle class instead. That's one thing nailed to the wall.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,6 @@
 $(document).ready(function(){
   $("p").click(function(){
-    $("p").addClass("highlight parabox");
+    $("p").toggleClass("highlight parabox");
   });
-  $(".highlight").click(function(){
-    $("p").removeClass("highlight parabox");
-    console.log("I'm here")
-  });
+
 });
